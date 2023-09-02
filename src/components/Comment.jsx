277898f781af5c667d183c4678d7b98a37c44229
Bao Qi <104255685+baoqii@users.noreply.github.com>
2023-09-02 22:18:05 +0800

    Add dark variant

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,7 @@
 const Comment = ({ author, text }) => {
   return (
-    <div className="p-6 m-0 border-b border-solid border-alto-200">
-      <span className="font-extrabold uppercase tracking-widest text-sm border-b-2 border-solid border-rock-blue-500/[.6] text-tundora-700 cursor-pointer transition-all duration-300 hover:text-rock-blue-500 hover:border-rock-blue-500">
+    <div className="p-6 m-0 border-b border-solid border-alto-200 dark:border-onyx-900">
+      <span className="font-extrabold uppercase tracking-widest text-sm border-b-2 border-solid border-rock-blue-500/[.6] dark:border-rock-blue-400 text-tundora-700 dark:text-silver-400 cursor-pointer transition-all duration-300 hover:text-rock-blue-500 dark:hover:text-rock-blue-300 hover:border-rock-blue-500 dark:hover:border-rock-blue-300">
         {author}:
       </span>
       <blockquote className="p-0 pl-5 border-l border-l-solid border-l-alto-200 ml-5 my-5 mr-0 uppercase tracking-widest text-sm">
