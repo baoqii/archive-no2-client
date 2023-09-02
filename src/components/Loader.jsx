7331c2888445b054952f4484406eb861dd08fe3b
Bao Qi <104255685+baoqii@users.noreply.github.com>
2023-09-02 22:22:39 +0800

    Center loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,16 +2,20 @@ import { ThreeDots } from "react-loader-spinner";
 
 const Loader = () => {
   return (
-    <ThreeDots
-      height="80"
-      width="80"
-      radius="9"
-      color="#4fa94d"
-      ariaLabel="three-dots-loading"
-      wrapperStyle={{}}
-      wrapperClassName=""
-      visible={true}
-    />
+    <section className="relative w-[700px] ml-[400px] ">
+      <div className="flex flex-col justify-center items-center h-[600px] gap-2">
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="#9ba5cb"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+        />
+      </div>
+    </section>
   );
 };
 
