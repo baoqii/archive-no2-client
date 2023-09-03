ad7af63de0bfe72f0ddf9b68308d1bb9e03c92f2
Bao Qi <104255685+baoqii@users.noreply.github.com>
2023-09-03 23:42:07 +0800

    Add tooltip for side menu links

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,41 +1,108 @@
+import { useState, useEffect } from "react";
 import { Compass, Home, Mail, User } from "react-feather";
 import { NavLink } from "react-router-dom";
+import { Tooltip } from "react-tooltip";
 
-const SideMenu = () => {
+const SideMenu = ({ isDarkMode }) => {
+  const [tooltipVariant, setTooltipVariant] = useState(
+    isDarkMode ? "light" : "dark"
+  );
+
+  // Listen for changes in the isDarkMode prop and update tooltipVariant accordingly
+  useEffect(() => {
+    setTooltipVariant(isDarkMode ? "light" : "dark");
+  }, [isDarkMode]);
+
   return (
     <nav className="sidemenu h-full w-[70px] top-0 gap-6 flex flex-col items-center justify-center left-0 fixed bg-white dark:bg-eerie-black-950 transition-all duration-300 ease-linear">
       <NavLink
         to="/"
         className="bg-transparent active:bg-rock-blue-500 dark:active-east-bay-800 text-rock-blue-500 dark:text-pigeon-post-400 active:text-white w-[45px] h-[45px] grid items-center place-items-center rounded-md transition-all duration-300 ease-linear cursor-pointer hover:text-white hover:bg-rock-blue-500 dark:hover:bg-east-bay-800"
+        data-tooltip-id="tt-home"
+        data-tooltip-content="HOME"
+        data-tooltip-place="right"
+        data-tooltip-variant={tooltipVariant}
       >
         <i className="inline-flex shrink-0 text-center">
           <Home size={22} />
         </i>
       </NavLink>
+      <Tooltip
+        id="tt-home"
+        noArrow
+        style={{
+          borderRadius: "6px",
+          letterSpacing: "0.1em",
+          padding: "4px 12px",
+          fontSize: "12px",
+        }}
+      />
       <NavLink
-        to="/"
+        to="/about"
         className="bg-transparent active:bg-rock-blue-500 dark:active:bg-east-bay-800 text-rock-blue-500 dark:text-pigeon-post-400 active:text-white w-[45px] h-[45px] grid items-center place-items-center rounded-md transition-all duration-300 ease-linear cursor-pointer hover:text-white hover:bg-rock-blue-500 dark:hover:bg-east-bay-800"
+        data-tooltip-id="tt-about"
+        data-tooltip-content="ABOUT"
+        data-tooltip-place="right"
+        data-tooltip-variant={tooltipVariant}
       >
         <i className="inline-flex shrink-0 text-center">
           <User size={22} />
         </i>
       </NavLink>
+      <Tooltip
+        id="tt-about"
+        noArrow
+        style={{
+          borderRadius: "6px",
+          letterSpacing: "0.1em",
+          padding: "4px 12px",
+          fontSize: "12px",
+        }}
+      />
       <NavLink
         to="/tags"
         className="bg-transparent active:bg-rock-blue-500 dark:active:bg-east-bay-800 text-rock-blue-500 dark:text-pigeon-post-400 active:text-white w-[45px] h-[45px] grid items-center place-items-center rounded-md transition-all duration-300 ease-linear cursor-pointer hover:text-white hover:bg-rock-blue-500 dark:hover:bg-east-bay-800"
+        data-tooltip-id="tt-navigate"
+        data-tooltip-content="NAVIGATE"
+        data-tooltip-place="right"
+        data-tooltip-variant={tooltipVariant}
       >
         <i className="inline-flex shrink-0 text-center">
           <Compass size={22} />
         </i>
       </NavLink>
+      <Tooltip
+        id="tt-navigate"
+        noArrow
+        style={{
+          borderRadius: "6px",
+          letterSpacing: "0.1em",
+          padding: "4px 12px",
+          fontSize: "12px",
+        }}
+      />
       <NavLink
-        to="/"
+        to="/newsletter"
         className="bg-transparent active:bg-rock-blue-500 dark:active:bg-east-bay-800 text-rock-blue-500 dark:text-pigeon-post-400 active:text-white w-[45px] h-[45px] grid items-center place-items-center rounded-md transition-all duration-300 ease-linear cursor-pointer hover:text-white hover:bg-rock-blue-500 dark:hover:bg-east-bay-800"
+        data-tooltip-id="tt-newsletter"
+        data-tooltip-content="NEWSLETTER"
+        data-tooltip-place="right"
+        data-tooltip-variant={tooltipVariant}
       >
         <i className="inline-flex shrink-0 text-center">
           <Mail size={22} />
         </i>
       </NavLink>
+      <Tooltip
+        id="tt-newsletter"
+        noArrow
+        style={{
+          borderRadius: "6px",
+          letterSpacing: "0.1em",
+          padding: "4px 12px",
+          fontSize: "12px",
+        }}
+      />
     </nav>
   );
 };
