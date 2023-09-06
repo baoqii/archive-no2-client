5b7b6fc79fd83588966e9be43eff71ba5fec52f9
Bao Qi <104255685+baoqii@users.noreply.github.com>
2023-09-06 10:42:26 +0800

    Add scrollToTopButton and Footer

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import SideMenu from "../components/SideMenu";
+import ScrollToTopButton from "../components/ScrollToTopButton";
+import { Footer } from "../components/Footer";
 
 const Layout = () => {
   return (
     <>
-      <main className="container relative mx-auto my-[50px] w-[calc(300px+700px+100px)]">
-        <SideMenu />
+      <main className="container relative mx-auto mt-[50px] mb-4 w-[calc(300px+700px+100px)]">
         <Sidebar />
+        <SideMenu />
+        <ScrollToTopButton />
         <Outlet />
+        <Footer />
       </main>
-      {/* {Footer} */}
     </>
   );
 };
