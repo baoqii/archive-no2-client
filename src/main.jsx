870f058ccd7dad3552a6baded8a5ea8dcbf5b87f
Bao Qi <104255685+baoqii@users.noreply.github.com>
2023-09-06 10:44:25 +0800

    Wrap app inside Theme and Search Provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./contexts/ThemeContext.jsx";
+import { SearchProvider } from "./contexts/SearchContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <SearchProvider>
+          <App />
+        </SearchProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
