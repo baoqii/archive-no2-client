029fb9da83a51a2b67ee9ad1b631926cba674ecb
Bao Qi <104255685+baoqii@users.noreply.github.com>
2023-09-11 00:58:10 +0800

    Add scroll to top functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,52 @@
 import "./App.css";
-import ErrorPage from "./pages/ErrorPage";
-
-import { Routes, Route } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage";
+import { Routes, Route, useLocation } from "react-router-dom";
 import PostDetail from "./pages/PostDetail";
 import Home from "./pages/Home";
 import TagPage from "./pages/TagPage";
 import Layout from "./pages/Layout";
+import PostsWithinATag from "./pages/PostsWithinATag";
+import { AnimatePresence } from "framer-motion";
+import FilteredPosts from "./pages/FilteredPosts";
+import About from "./pages/About";
+import Newsletter from "./pages/Newsletter";
+import { useEffect } from "react";
 
-// About , Newsletter, Tags page, no results found,
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/posts/:post_id" element={<PostDetail />} />
-          <Route path="/tags" element={<TagPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
+      <ScrollToTop />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/page/:currentPage" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/newsletter" element={<Newsletter />} />
+            <Route path="/posts/:post_id" element={<PostDetail />} />
+            <Route path="/search" element={<FilteredPosts />} />
+            <Route path="/tags" element={<TagPage />} />
+            <Route path="/tag/:tag_id" element={<PostsWithinATag />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
