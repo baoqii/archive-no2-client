f67a462cce073bbb67d1c3f00d34544f75654c4d
Bao Qi <104255685+baoqii@users.noreply.github.com>
2023-09-10 10:51:50 +0800

    Add toast

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,8 +3,13 @@ import Sidebar from "../components/Sidebar";
 import SideMenu from "../components/SideMenu";
 import ScrollToTopButton from "../components/ScrollToTopButton";
 import { Footer } from "../components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const Layout = () => {
+  const { isDarkMode } = useContext(ThemeContext);
   return (
     <>
       <main className="container relative mx-auto mt-[50px] mb-4 w-[calc(300px+700px+100px)]">
@@ -13,6 +18,18 @@ const Layout = () => {
         <ScrollToTopButton />
         <Outlet />
         <Footer />
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable={false}
+          pauseOnHover={false}
+          theme={isDarkMode ? "dark" : "light"}
+        />
       </main>
     </>
   );
